74dc6c7abdbb11cce3854583c629daee8e6587f8
bsher morjan <bshermorgan4@gmail.com>
2026-04-19 10:52:46 +0300

    create server failure from dio error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app2/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app2/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly_app2/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final HomeLocalDataSource homeLocalDataSource;
@@ -16,30 +17,38 @@ class HomeRepoImpl extends HomeRepo {
   @override
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
     try {
-      var booksList = homeLocalDataSource.fetchFeaturedBooks();
-      if (booksList.isEmpty) {
-        return right(booksList);
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isEmpty) {
+        return right(books);
       }
-      var books = await homeRemoteDataSource.fetchFeaturedBooks();
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
 
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.formDioException(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
     }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks()async {
-   try {
-      var booksList = homeLocalDataSource.fetchNewestBooks();
-      if (booksList.isEmpty) {
-        return right(booksList);
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isEmpty) {
+        return right(books);
       }
-      var books = await homeRemoteDataSource.fetchNewestBooks();
+      books = await homeRemoteDataSource.fetchNewestBooks();
 
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.formDioException(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
     }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,63 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMessage;
+
+  Failure({required this.errMessage});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errMessage});
+
+  factory ServerFailure.formDioException(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(errMessage: 'Connection timeout with the server');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(errMessage: 'Send timeout with the server');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(errMessage: 'Receive timeout with the server');
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure(errMessage: 'Bad SSL certificate');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          dioError.response!.statusCode!,
+          dioError.response!.data,
+        );
+
+      case DioExceptionType.cancel:
+        return ServerFailure(errMessage: 'Request to the server was canceled');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure(errMessage: 'No Internet connection');
+
+      case DioExceptionType.unknown:
+        if (dioError.message?.contains('SocketException') ?? false) {
+          return ServerFailure(errMessage: 'No Internet Connection');
+        }
+        return ServerFailure(errMessage: 'Unexpected Error, Please try again!');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(errMessage: response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure(
+        errMessage: 'Your request not found, please try later',
+      );
+    } else if (statusCode == 500) {
+      return ServerFailure(
+        errMessage: 'Internal Server error , please try later ',
+      );
+    } else {
+      return ServerFailure(
+        errMessage: 'Opps, There was an Error , please try again',
+      );
+    }
+  }
+}
